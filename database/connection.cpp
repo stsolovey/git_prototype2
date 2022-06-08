5b367e401e42b03d598d89ac4eb2adf0ca97d29e
stsolovey <50598962+stsolovey@users.noreply.github.com>
2022-06-08 16:40:21 +0300

    credentials removing

--- database/connection.cpp
+++ database/connection.cpp
@@ -11,11 +11,11 @@ Connection::~Connection()
 bool Connection::openDataBase()
 {
     QSqlDatabase db = QSqlDatabase::addDatabase("QPSQL");
-    db.setHostName("rc1b-6fuig7zk82eezg7l.mdb.yandexcloud.net");
+    db.setHostName("******.mdb.yandexcloud.net");
     db.setPort(6432);
     db.setDatabaseName("yaPsSql");
-    db.setUserName("user1");
-    db.setPassword("12345678");
+    db.setUserName("******");
+    db.setPassword("********");
     if (!db.open()) {
         qDebug() << "Ошибка БД" <<  db.lastError().text();
         return false;
